d15cc3783c03dcb3771b3d2d13a946e12600aa44
WhiteKeep <60810530+WhiteKeep@users.noreply.github.com>
2020-02-27 21:43:06 +0300

    Add files via upload
    edit cofig way
    remove emoji role test

--- bot.py
+++ bot.py
@@ -4,22 +4,41 @@ import discord
 from discord import utils
 
 
-import config
+production = True
+
+
+if production is not True:
+
+    import configer
+
+    DisToken = str(configer.DisToken)
+    debug = bool(configer.debug)
+    actData = str(configer.actData)
+    servId = int(configer.servId)
+    roleId = int(configer.roleId)
+    voiceID = int(configer.voiceID)
+    categoryID = int(configer.categoryID)
+    prefix = str(configer.prefix)
+    helpName = str(configer.helpName)
+    helpData01 = str(configer.helpData01)
+    #helpData02 = str(configer.helpData02)
+else:
+    import config
+
+    DisToken = str(config.DisToken)
+    debug = bool(config.debug)
+    actData = str(config.actData)
+    servId = int(config.servId)
+    roleId = int(config.roleId)
+    voiceID = int(config.voiceID)
+    categoryID = int(config.categoryID)
+    prefix = str(config.prefix)
+    helpName = str(config.helpName)
+    helpData01 = str(config.helpData01)
+    #helpData02 = str(config.helpData02)
 
 
 client = discord.Client()
-
-DisToken = str(config.DisToken)
-debug = bool(config.debug)
-actData = str(config.actData)
-servId = int(config.servId)
-roleId = int(config.roleId)
-voiceID = int(config.voiceID)
-categoryID = int(config.categoryID)
-prefix = str(config.prefix)
-helpName = str(config.helpName)
-helpData01 = str(config.helpData01)
-#helpData02 = str(config.helpData02)
 
 
 @client.event
@@ -45,7 +64,7 @@ async def on_message(message):
     mess = message.content[:]
 
     if mess == helpName:
-        await message.channel.send(helpData01)
+        await message.channel.send(helpData01 + '`' + prefix + '`')
         #await message.channel.send(helpData02)
 
     role_pio = client.get_guild(servId).get_role(roleId)
@@ -62,27 +81,8 @@ async def on_message(message):
                 i = 0
         if t == len(prefix):
 
-            if mess[len(prefix):] == 'add':
-                if debug is True:
-                    print('embed add now')
-
-
-                #emb = discord.Embed(name = 'Pypis', color = 0x39d0d6)
-                #emb.add_field(name = 'Пиу ', value = 'пиу пиgggghughughughufghrufgughfu', inline = False)
-
-
-                
-
-
-
-
-
-                #await message.channel.send(embed=emb)
-            else:
-                mtext = mess[len(prefix):]
-                await message.channel.send(mtext)
-
-
+            mtext = mess[len(prefix):]
+            await message.channel.send(mtext)
 
 
 
@@ -101,60 +101,6 @@ async def on_voice_state_update(member, before, after):
     for channel in client.get_channel(voiceID).category.voice_channels:
         if(channel.id == voiceID or len(channel.members) != 0): continue
         await channel.delete(reason="В голосовой комнате 0 людей!")
-
-
-
-messageID = 681451205737971791
-
-@client.event
-async def on_raw_reaction_add(payload):
-    print('event')
-    message_id = payload.message_id
-    if message_id == messageID:
-        print('message indetifical')
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-
-        if payload.emoji.name == 'fit':
-            role = discord.utils.get(guild.roles, name = 'Читатель')
-        elif payload.emoji.name == 'io':
-            role = discord.utils.get(guild.roles, name = 'Зритель')
-        
-        if role is not None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.add_roles(role)
-                print('done')
-            else:
-                print('member not found')
-        else:
-            print('role not found')
-
-
-
-@client.event
-async def on_raw_reaction_remove(payload):
-    print('event')
-    message_id = payload.message_id
-    if message_id == messageID:
-        print('message indetifical')
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-
-        if payload.emoji.name == 'fit':
-            role = discord.utils.get(guild.roles, name = 'Читатель')
-        elif payload.emoji.name == 'io':
-            role = discord.utils.get(guild.roles, name = 'Зритель')
-        
-        if role is not None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.remove_roles(role)
-                print('done')
-            else:
-                print('member not found')
-        else:
-            print('role not found')
 
 
 
